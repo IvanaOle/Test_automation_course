72bdb35f40c49a48fe4dc4991bb781b079ab83c0
ivana.oleksakova <ivana.oleksakova@itera.no>
2019-06-18 15:21:40 +0200

    Add action - check click and hold button

--- src/TestKalkulacka/SemaforTest.java
+++ src/TestKalkulacka/SemaforTest.java
@@ -24,27 +24,34 @@ public class SemaforTest {
     public void test(){
         String expectedGreen = "rgba(10, 129, 0, 1)";
         String expectedRed = "rgba(205, 58, 63, 1)";
+        String expectedOrange = "rgba(191, 111, 7, 1)";
 
         driver.get(BASE_URL);
 
         WebElement trafficLight = driver.findElement(By.xpath("//div[@class='light']"));
         System.out.println(trafficLight.getCssValue("background-color"));
         String redcolor = trafficLight.getCssValue("background-color");
+        Actions action = new Actions(driver);
+
         //Classic red color as background color
         Assert.assertEquals(expectedRed,redcolor);
+
         //When hover is applied - green color TRIEDA Actions!!! vykonava akciu
-        Actions action = new Actions(driver);
         action.moveToElement(trafficLight).build().perform();
         String actualGreenColor = trafficLight.getCssValue("background-color");
         Assert.assertEquals(expectedGreen,actualGreenColor);
 
-
+        //Active when it is orange
+        action.clickAndHold(trafficLight).build().perform(); //pozor na toto - vzdy musi byt build a perform
+        String actualOrangeColor = trafficLight.getCssValue("background-color");
+        System.out.println(actualOrangeColor);
+        Assert.assertEquals(expectedOrange,actualOrangeColor);
 
     }
     @After
     public void tearDown(){
-        //driver.close();
-        //driver.quit();
+        driver.close();
+        driver.quit();
 
     }
 }
