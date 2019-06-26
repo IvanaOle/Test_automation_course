dad9923e531d80511c761caaa7ff0494303e5c9d
ivana.oleksakova <ivana.oleksakova@itera.no>
2019-06-26 09:22:37 +0200

    Add: Calculator - methods extract checkSum checkConduct

--- src/TestKalkulacka/TestCalculator.java
+++ src/TestKalkulacka/TestCalculator.java
@@ -12,38 +12,36 @@ public class TestCalculator {
     private String BASE_URL = "http:/localhost:8888/kalkulacka.php";
     private WebDriver driver;
 
+
     @Before
     public void setUp(){
         driver = new FirefoxDriver();
+        driver.get(BASE_URL);
+        driver.manage().window().maximize();
     }
+
 
     @Test
     public void testSum(){
-        driver.get(BASE_URL);
-        driver.findElement(By.id("firstInput")).sendKeys("5");
-        driver.findElement(By.id("secondInput")).sendKeys("2");
-        driver.findElement(By.id("count")).click();
-
-    System.out.println(driver.findElement(By.id("result")).getText());
-
+        checkSum("1", "2", "count", "3");
+        checkSum("100", "20", "count", "120");
+        checkSum("8", "2", "count", "10");
+        checkSum("-100", "100", "count", "0");
     }
+
 
     @Test
     public void testDeduc(){
-
+        checkDeduct("1", "2", "deduct", "-1");
+        checkDeduct("100", "20", "deduct", "80");
+        checkDeduct("-1", "-2", "deduct", "1");
+        checkDeduct("-50", "60", "deduct", "-110");
+        checkDeduct("80", "2", "deduct", "78");
     }
 
     @Test
     public void testReset(){
-        driver.get(BASE_URL);
-        driver.findElement(By.id("firstInput")).sendKeys("1");
-        driver.findElement(By.id("secondInput")).sendKeys("2");
-        driver.findElement(By.id("count")).click();
-        String result = driver.findElement(By.id("result")).getText();
-        Assert.assertEquals("3",result);
 
-        driver.findElement(By.id("reset")).click();
-        Assert.assertEquals("",driver.findElement(By.id("result")).getText());
 
     }
 
@@ -57,5 +55,16 @@ public class TestCalculator {
         driver.close();
         driver.quit();
 
+    }
+    private void checkSum(String firstInput, String secondInput, String count, String s3) {
+        checkDeduct(secondInput, firstInput, count, s3);
+    }
+    private void checkDeduct(String s, String s2, String deduct, String s3) {
+        driver.findElement(By.id("firstInput")).clear();
+        driver.findElement(By.id("firstInput")).sendKeys(s);
+        driver.findElement(By.id("secondInput")).clear();
+        driver.findElement(By.id("secondInput")).sendKeys(s2);
+        driver.findElement(By.id(deduct)).click();
+        Assert.assertEquals(s3, driver.findElement(By.id("result")).getText());
     }
 }
