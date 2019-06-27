ecba2e574a3ae7b14fb7dd5a86a6fca513b8be69
ivana.oleksakova <ivana.oleksakova@itera.no>
2019-06-27 15:49:16 +0200

    refactor: add extended maintest to ClickMeBaby,CssSelector and FullClickMeBaby TC

--- src/TestKalkulacka/ClickMeBabyTests.java
+++ src/TestKalkulacka/ClickMeBabyTests.java
@@ -1,32 +1,22 @@
 package TestKalkulacka;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class ClickMeBabyTests {
+public class ClickMeBabyTests extends MainTest{
     private WebDriver driver;
-    private String Base_URL = "http:/localhost:8888/clickmebaby.php";
 
     @Before
-    public void SetUp(){
-        driver = new FirefoxDriver();
+    public void openBaseUrl(){
+        driver.get(getBASE_URL()+ "clickmebaby.php");
     }
     @Test
     public void Test(){
-        driver.get(Base_URL);
         driver.findElement(By.cssSelector("button")).click();
         driver.findElement(By.cssSelector("div.container button")).click();
         driver.findElement(By.cssSelector("div.row>button")).click();
     }
-
-    @After
-    public void tearDown(){
-        driver.quit();
-    }
-
 
 }
--- src/TestKalkulacka/CssSelector.java
+++ src/TestKalkulacka/CssSelector.java
@@ -1,23 +1,17 @@
 package TestKalkulacka;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class CssSelector {
-
-    private WebDriver driver;
-    private String Base_URL = "http://localhost:8888/kalkulacka.php";
+public class CssSelector extends MainTest{
 
     @Before
-    public void SetUp() {
-        driver = new FirefoxDriver();
+    public void openUrl(){
+        driver.get(getBASE_URL() + "stroopeffect.php");
     }
+
     @Test
     public void Test() {
-        driver.get(Base_URL);
         driver.findElement(By.cssSelector("button#count")).click();
         driver.findElement(By.cssSelector("#count")).click();//oznacenie ID #
         driver.findElement(By.cssSelector("button.btn-success")).click();//oznacenie triedy - class sa deje bodkou
@@ -25,9 +19,6 @@ public class CssSelector {
     driver.findElement(By.cssSelector("div.row>button")).click(); //tu si najde najprv classu div.row a v nom prvy button
     driver.findElement(By.cssSelector("div.container button")).click();//priame dieta rodica sa oznaci ako >
     }
-    @After
-    public void tearDown(){
-        driver.quit();
-    }
+
 }
 
